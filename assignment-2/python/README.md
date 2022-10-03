6f26d94cb58b92176f127f8185a951defd9d8a09
wfl32 <wfl32@drexel.edu>
2022-10-03 15:35:43 -0400

    Path instruction added

--- assignment-2/python/README.md
+++ assignment-2/python/README.md
@@ -4,9 +4,10 @@ wfl32_assignment-2_customCube.py
 
 Function:
 Type in 3 integer inputs with space in between.
+Type in desktop path.
 
 For example:
-10 26 76
+10 26 76 C:\Users\willr\Desktop\
 
-This will create a cube with those 3 numbers as its dimensions as (WIDTH, HEIGHT, DEPTH).
+This will create a cube with those 3 numbers as its dimensions as (WIDTH, HEIGHT, DEPTH) to your designated path location.
 '''
--- assignment-2/python/wfl32_assignment-2_customCube.py
+++ assignment-2/python/wfl32_assignment-2_customCube.py
@@ -1,3 +1,4 @@
+import string
 import maya.standalone
 maya.standalone.initialize()
 
@@ -7,19 +8,19 @@ parser = argparse.ArgumentParser(description='This script creates a bunch of cub
 parser.add_argument('cube_width', type=int, help="Value of cube width")
 parser.add_argument('cube_height', type=int, help="Value of cube height")
 parser.add_argument('cube_depth', type=int, help="Value of cube depth")
+parser.add_argument('desktopPath', type=string, help="String Path to desktop")
 args = parser.parse_args()
 
 import maya.cmds as mc
+
+pathInput = string(args.desktopPath)
 
 mc.polyCube(w = args.cube_width, h = args.cube_height, d = args.cube_depth)
 print("Your custom cube has been created!")
 
 #The "r" before the path converts string from a normal string to a RAW string.
-
-customPth = r"C:\Users\willr\Desktop\yourCube_01.ma"
+customPth = r(pathInput)
 
 mc.file(rename=customPth)
 mc.file(save=True, type="mayaAscii")
 print("Your cube is saved as 'yourCube_01.ma' to your desktop.")
-
-
